563544ba68228c5fb5bece0c5888367b9276d9a1
hariprostack123 <128031941+hariprostack123@users.noreply.github.com>
2023-11-15 16:43:25 +0530

    js3

---
+++ index.js
@@ -0,0 +1,33 @@
+/*
+  arithmentic expression is a combination of
+  operands (values, variables, etx.)
+  operators (+ - * / %)
+  that can be evaluated to a value
+  ex. y = x + 5;
+*/
+
+let students = 20;
+
+//students = students + 1;
+//students = students - 1;
+//students = students * 2;
+//students = students / 2;
+//let extraStudents = students % 2;
+
+//students += 1;
+//students -= 1;
+//students *= 2;
+//students /= 2;
+//console.log(extraStudents);
+
+ /*
+    operator precedence
+    1. parenthesis ()
+    2. exponents 
+    3. multiplication & division
+    4. addition & subtraction
+*/
+
+let result = (1 + 2) * (3 + 4);
+
+console.log(result);
